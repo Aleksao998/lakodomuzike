4e08bd5dc3d3376f38cd02229468503f236f800a
svetagajic <svetagajic@gmail.com>
2020-04-30 13:08:39 +0200

    MusicianRoutesAdded

--- Faza 6/BackEnd/routes/employer-routes.js
+++ Faza 6/BackEnd/routes/employer-routes.js
@@ -1,16 +1,20 @@
-const express = require('express');
+const express = require("express");
 const {
-    getEmployers,
-    getEmployer,
-    createEmployer,
-    updateEmployer,
-    deleteEmployer
-} = require('../controllers/employer-controllers');
+	getEmployers,
+	getEmployer,
+	createEmployer,
+	updateEmployer,
+	deleteEmployer,
+} = require("../controllers/employer-controllers");
 
 const router = express.Router();
 
-router.route('/').get(getEmployers).post(createEmployer);
+router.route("/").get(getEmployers).post(createEmployer);
 
-router.route('/:id').get(getEmployer).put(updateEmployer).delete(deleteEmployer);
+router
+	.route("/:id")
+	.get(getEmployer)
+	.put(updateEmployer)
+	.delete(deleteEmployer);
 
 module.exports = router;
--- Faza 6/BackEnd/server.js
+++ Faza 6/BackEnd/server.js
@@ -1,34 +1,37 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const colors = require('colors');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 //Load env vars
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
 
 connectDB();
 
 //Route files
-const employers = require('./routes/employer-routes');
+const employers = require("./routes/employer-routes");
+const musicians = require("./routes/musician-routes");
 
 const app = express();
 
 // Dev logging middleware
-if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
 }
-// Mount routers 
-app.use('/api/v1/employer', employers);
-
+// Mount routers
+app.use("/api/v1/employer", employers);
+app.use("/api/v1/musician", musicians);
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
 	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	)
 );
 
 //Handle unhandled rejections
