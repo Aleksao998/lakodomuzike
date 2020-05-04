e8a6cd02d4e82833ebf35abad0da6553394892ce
Sara Lablo <sara.labalo18@gmail.com>
2020-05-04 17:53:38 +0200

    login-page

--- Faza 6/FrontEnd/src/views/LoginPage/LoginPage.js
+++ Faza 6/FrontEnd/src/views/LoginPage/LoginPage.js
@@ -1,9 +1,51 @@
 import React from "react";
+// reactstrap components
+import { Button, Card, Form, Input, Container, Row, Col } from "reactstrap";
 
 function LoginPage() {
+  document.documentElement.classList.remove("nav-open");
+
+  React.useEffect(() => {
+    document.body.classList.add("register-page");
+    return function cleanup() {
+      document.body.classList.remove("register-page");
+    };
+  });
   return (
     <>
-      <div></div>
+      <div class="container">
+        <div class="row">
+          <div class=" cont">
+            <div class="form sign-in">
+              <h2>Dobrodošli</h2>
+              <label>
+                <span>Email</span>
+                <input type="email" />
+              </label>
+              <label>
+                <span>Password</span>
+                <input type="password" />
+              </label>
+              <button type="button" class="submit">
+                Sign In
+              </button>
+            </div>
+            <div class="sub-cont">
+              <div class="img">
+                <div class="img__text m--up"></div>
+                <div class="img__text m--up">
+                  <h2>New here?</h2>
+                  <p>Sign up and discover great amount of new opportunities!</p>
+                </div>
+
+                <div class="img__btn">
+                  <span class="m--up">Sign Up</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- Faza 6/FrontEnd/src/views/RegistrationPage/RegistrationEmployer/RegistrationEmployerPage.js
+++ Faza 6/FrontEnd/src/views/RegistrationPage/RegistrationEmployer/RegistrationEmployerPage.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { Button, Card, Form, Input, Container, Row, Col } from "reactstrap";
 
 function RegisterPageEmployer() {
+  document.documentElement.classList.remove("nav-open");
+  React.useEffect(() => {
+    document.body.classList.add("register-page");
+    return function cleanup() {
+      document.body.classList.remove("register-page");
+    };
+  });
   return (
     <>
-      <div></div>
+      <div class="container">
+        <div class="row">
+          <div class="cont">
+            <div class="col-9 form sign-in">
+              <p>Sara</p>
+            </div>
+            <div class="col-3 sub-cont">
+              <p>Aleksa</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
