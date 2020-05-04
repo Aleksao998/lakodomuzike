3471ec14a2a6f4cffef8b9bb6c10bf5c57b6ee70
AleksaOpacic <aleksaopacic988@gmail.com>
2020-05-04 03:12:31 +0200

    Poslodavac i oglasi faza 0

---
+++ Faza 6/FrontEnd/src/views/AdsPage/AdsHeader/AdsHeader.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function AdsHeader() {
+  return (
+    <div class="slider-area ">
+      <div
+        class="single-slider section-overly slider-height2 d-flex align-items-center"
+        data-background="assets/img/hero/about.jpg"
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-12">
+              <div class="hero-cap text-center">
+                <h2>Pronadji Oglas</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdsHeader;
